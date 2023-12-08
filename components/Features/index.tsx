f09a4ba5073289cd4b4a651bcd6ceff2d6994f49
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-12-08 19:28:46 +0300

    first commit

---
+++ components/Features/index.tsx
@@ -0,0 +1,29 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="Electoral Commitments"
+            paragraph={
+              "Building Tomorrow's Community Together: As your MCA, I pledge to prioritize education reform, fostering local entrepreneurship, and improving healthcare accessibility. My commitment revolves around transparent governance, sustainable development, and enhancing infrastructure for a vibrant and inclusive community. Together, let's create a future we can all be proud of."
+            }
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
